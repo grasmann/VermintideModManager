b5a0b39f054ede605dc6bf090021b2e077f7bc91
Grasmann <grasmann1805@gmail.com>
2017-05-05 10:08:08 +0200

    Fixes

--- VMM/VMM/My Project/Resources.Designer.vb
+++ VMM/VMM/My Project/Resources.Designer.vb
@@ -113,16 +113,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property info_256() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("info_256", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property install_16() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("install_16", resourceCulture)
