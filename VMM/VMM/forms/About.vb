e46b616b4023510c04afbaecd558ae76044d6cc4
Grasmann <grasmann1805@gmail.com>
2017-05-05 10:28:57 +0200

    blubb

--- VMM/VMM/forms/About.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class About
-
-    Private Sub About_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        lbl_version.Text = Version.Current
-    End Sub
-
-End Class
--- VMM/VMM/forms/Modules/ReadMe.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class ReadMe
-
-End Class
