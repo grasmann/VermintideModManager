33b8d82cbabb5644ae6b6a0e2baf2b5642f34fa0
Grasmann <grasmann1805@gmail.com>
2017-05-08 22:22:07 +0200

    Options; Source Overwrite;

---
+++ VMM/forms/Options.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Options
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.lbl_mod_repository_path = New System.Windows.Forms.Label()
+        Me.grp_mod_repository = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btn_browse_mod_repository = New System.Windows.Forms.Button()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.grp_mod_repository.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(464, 387)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.grp_mod_repository)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(456, 361)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Development"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'lbl_mod_repository_path
+        '
+        Me.lbl_mod_repository_path.AutoSize = True
+        Me.lbl_mod_repository_path.Location = New System.Drawing.Point(6, 26)
+        Me.lbl_mod_repository_path.Name = "lbl_mod_repository_path"
+        Me.lbl_mod_repository_path.Size = New System.Drawing.Size(29, 13)
+        Me.lbl_mod_repository_path.TabIndex = 0
+        Me.lbl_mod_repository_path.Text = "Path"
+        '
+        'grp_mod_repository
+        '
+        Me.grp_mod_repository.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.grp_mod_repository.Controls.Add(Me.btn_browse_mod_repository)
+        Me.grp_mod_repository.Controls.Add(Me.TextBox1)
+        Me.grp_mod_repository.Controls.Add(Me.lbl_mod_repository_path)
+        Me.grp_mod_repository.Location = New System.Drawing.Point(8, 6)
+        Me.grp_mod_repository.Name = "grp_mod_repository"
+        Me.grp_mod_repository.Size = New System.Drawing.Size(440, 56)
+        Me.grp_mod_repository.TabIndex = 1
+        Me.grp_mod_repository.TabStop = False
+        Me.grp_mod_repository.Text = "Source Overwrite"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(84, 23)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(299, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'btn_browse_mod_repository
+        '
+        Me.btn_browse_mod_repository.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn_browse_mod_repository.Location = New System.Drawing.Point(389, 21)
+        Me.btn_browse_mod_repository.Name = "btn_browse_mod_repository"
+        Me.btn_browse_mod_repository.Size = New System.Drawing.Size(45, 23)
+        Me.btn_browse_mod_repository.TabIndex = 2
+        Me.btn_browse_mod_repository.Text = "..."
+        Me.btn_browse_mod_repository.UseVisualStyleBackColor = True
+        '
+        'Options
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(464, 387)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Options"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Options"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.grp_mod_repository.ResumeLayout(False)
+        Me.grp_mod_repository.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents grp_mod_repository As GroupBox
+    Friend WithEvents lbl_mod_repository_path As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btn_browse_mod_repository As Button
+End Class
---
+++ VMM/forms/Options.vb
@@ -0,0 +1,52 @@
+﻿Public Class Options
+
+    Public Event SaveSettings()
+    Public Event RequestBrowseFolder()
+
+    Public Sub New(Args As main.ModuleArgs)
+        InitializeComponent()
+        TextBox1.Text = Args.Settings.SourceOverwrite
+    End Sub
+
+    Public Sub BrowseFolder(Args As main.ModuleArgs)
+        open_dialog(Args)
+    End Sub
+
+    Private Sub btn_browse_mod_repository_Click(sender As Object, e As EventArgs) Handles btn_browse_mod_repository.Click
+        RaiseEvent RequestBrowseFolder()
+
+        'If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+        '    Try
+        '        myStream = openFileDialog1.OpenFile()
+        '        If (myStream IsNot Nothing) Then
+        '            ' Insert code to read the stream here.
+        '        End If
+        '    Catch Ex As Exception
+        '        MessageBox.Show("Cannot read file from disk. Original error: " & Ex.Message)
+        '    Finally
+        '        ' Check this again, since we need to make sure we didn't throw an exception on open.
+        '        If (myStream IsNot Nothing) Then
+        '            myStream.Close()
+        '        End If
+        '    End Try
+        'End If
+
+    End Sub
+
+    Private Sub open_dialog(Args As main.ModuleArgs)
+        Dim dialog As New OpenFileDialog()
+        dialog.InitialDirectory = Args.Settings.SourceOverwrite
+        If String.IsNullOrEmpty(dialog.InitialDirectory) Then dialog.InitialDirectory = Application.StartupPath
+        dialog.Filter = "vermintideconsole.dll (vermintideconsole.dll)|vermintideconsole.dll"
+        If dialog.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Dim Folder As String = PathHelper.ExtractFolder(dialog.FileName)
+            If My.Computer.FileSystem.DirectoryExists(Folder) Then
+                If PathHelper.HasFiles(Folder) Then
+                    TextBox1.Text = Folder
+                    Args.Settings.SourceOverwrite = Folder
+                End If
+            End If
+        End If
+    End Sub
+
+End Class
