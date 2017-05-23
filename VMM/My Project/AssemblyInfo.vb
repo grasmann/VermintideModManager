18c682a38271d6234796a60b39e589e78e104814
grasmann <grasmann1805@gmail.com>
2017-05-23 04:38:31 +0200

    Updated assembly info;

--- VMM/My Project/AssemblyInfo.vb
+++ VMM/My Project/AssemblyInfo.vb
@@ -8,10 +8,10 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("VMM")>
+<Assembly: AssemblyTitle("Vermintide Mod Manager")>
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("VMM")>
+<Assembly: AssemblyProduct("Vermintide Mod Manager")>
 <Assembly: AssemblyCopyright("Copyright ©  2017")>
 <Assembly: AssemblyTrademark("")>
 
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("0.5.23.0")>
+<Assembly: AssemblyFileVersion("0.5.23.0")>
